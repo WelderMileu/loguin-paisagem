5ede3d57a97a22d2bc2fcac5f2dc370c09c58063
Welder Mileu <welderfelix21@gmail.com>
2019-09-28 02:24:51 -0300

    Dando vida ao formulario com javascript

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function(){
+	const $ = (e) =>  document.querySelector(e);
+
+
+	$("#submit").addEventListener("click", (event) => {
+		if($("#user").value == "" || $("#pass").value == ""){
+			event.preventDefault();
+			$(".warning").style.transform = "translate(-20%, 300%)";
+			$(".warning").style.opacity = "1";
+			$(".warning").style.border = "1px solid var(--vermelho)";
+			$(".warning").style.background = "var(--vermelhoClaro)";
+			$(".warning").style.textShadow = "1px 1px var(--vermelho)"
+			$(".warning").innerHTML = "Usuario ou Senha Vazios";
+			if ($("#user").value == "") {
+				$(".fa-user-o").style.color = "var(--vermelho)";
+				$(".fa-user-o").style.textShadow = "1px 5px 5px var(--vermelhoClaro)";
+			}else{
+				$(".fa-user-o").style.color = "var(--azul)";
+				$(".fa-user-o").style.textShadow = "none";
+			}
+
+			if ($("#pass").value == "") {
+				$(".fa-key").style.color = "var(--vermelho)";
+				$(".fa-key").style.textShadow = "1px 5px 5px var(--vermelhoClaro)";
+			}else{
+				$(".fa-key").style.color = "var(--azul)";
+				$(".fa-key").style.textShadow = "none";
+			}
+		}else{
+			event.preventDefault();
+			$(".warning").style.transform = "translate(-20%, 300%)";
+			$(".warning").style.opacity = "1";
+			$(".warning").innerHTML = "Loguin efetuado com sucesso";
+			$(".warning").style.border = "1px solid var(--branco)";
+			$(".warning").style.background = "var(--verde)";
+			$(".warning").style.textShadow = "1px 1px var(--verdelight)";
+		}
+	})
+})();
